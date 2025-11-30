1e95d3ca1f6405f14bf980c3db54ff49dc053688
SeaumSiddiqui <seaumsiddiqui@yahoo.com>
2025-11-30 09:51:18 +0600

    updated docker-compose.yml and init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,10 +1,15 @@
-CREATE DATABASE IF NOT EXISTS kcdb;
-CREATE DATABASE IF NOT EXISTS qcdb;
+-- ./mysql/init.sql
+-- Create Keycloak DB + user (idempotent)
+CREATE DATABASE IF NOT EXISTS kcdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER IF NOT EXISTS 'keycloak_user'@'%' IDENTIFIED BY 'KEYCLOAK_wgZjpIxG1vGuN7am03c';
 GRANT ALL PRIVILEGES ON kcdb.* TO 'keycloak_user'@'%';
+
+-- Create QCDB + user (with utf8mb4 for Bengali)
+CREATE DATABASE IF NOT EXISTS qcdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER IF NOT EXISTS 'spring_user'@'%' IDENTIFIED BY 'SPRING_wgZjpIxG1vGuN7am03c';
 GRANT ALL PRIVILEGES ON qcdb.* TO 'spring_user'@'%';
 
 FLUSH PRIVILEGES;
+
